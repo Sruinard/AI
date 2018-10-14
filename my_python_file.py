0b42d4e3ddd60877156c99d3ad3268ce960a6f86
Stef <stefruinard@hotmail.com>
2018-10-14 19:17:44 +0200

    added some tf

--- my_python_file.py
+++ my_python_file.py
@@ -1,9 +1,13 @@
-a=12
-b=23
-print(a+b)
-c=9000
-print('hell')
+import tensorflow as tf 
+import numpy as np
+
+print('hello')
+placeholder = tf.placeholder(dtype=tf.float32, shape=[None,5])
+layer = tf.layers.dense(inputs=placeholder, units=100)     
+a = 12
+b = 33
 
 def multiply(x,y):
     return x*y
 
+c = multiply(a,b)
