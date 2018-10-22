318eac6cb467d8691a0d80ced37b5b51121db25f
Stef <stefruinard@hotmail.com>
2018-10-22 13:43:53 +0200

    full pipeline

--- DynamicsModel/pipeline.py
+++ DynamicsModel/pipeline.py
@@ -4,96 +4,123 @@ import os
 import pandas as pd
 from sklearn import preprocessing
 from sklearn import pipeline
+import itertools
 
 data = pd.read_csv('/Users/stefruinard/Documents/ML6/DataECC/exp3_010.csv')
 
-x_train = data.loc[:1000, :]
-x_test = data.loc[1000:1500, :]
+x_train = data.loc[2000000:2020000, :]
+x_test = data.loc[2020000:2030000, :]
 
 
 class Preprocessing():
-    def __init__(self, X_train, X_test, batch_size, history_length, skip_n_frames=1, time_window=1):
+    def __init__(self, X_train, X_test, batch_size, skip_n_frames=1, mean_window=1, lag_period=0, keep_all=False,
+                 use_default=True):
         self.X_train = X_train
         self.X_test = X_test
-        self.skip_n_frames = skip_n_frames
-        self.time_window = time_window
-        self.batch_size = batch_size * np.max()
-        self.history_length = history_length
+        self.skip_n_frames = np.max((1, skip_n_frames))
+        self.mean_window = np.max((1, mean_window))
+        self.lag_period = lag_period
+        self.batch_size = batch_size * self.skip_n_frames * self.mean_window + np.max((0, lag_period)) * self.skip_n_frames * self.mean_window
         self.default_columns = ['V_source', 'I_U', 'I_V', 'I_W', 'sensor_torque', 'encoder_rpm', 'temperature_board']
+        self.keep_all = keep_all
+        self.use_default = use_default
 
-    def _remove_control_columns(self, data, keep_all=False):
+    def _remove_control_columns(self, data):
         # USES PANDAS
-        if keep_all:
+        if self.keep_all:
             data.loc[:, ['time']] = np.arange(data.shape[0])
         else:
             data = data.loc[:, data.columns[:11]]
-            data.iloc[:, ['time']] = np.arange(data.shape[0])
+            data.loc[:, ['time']] = np.arange(data.shape[0])
+            self.all_columns = data.columns
         return data
 
-    def _standardize(self, data, use_default=True):
-        #USES PANDAS
-        if use_default==False:
-            self.default_columns = list(input('which columns do you want to standardize?, type "default" for using the default columns', 'choose between:', str(data.columns)))
+    def _standardize(self, data):
+        # USES PANDAS
+        if self.use_default == False:
+            self.default_columns = list(
+                input('which columns do you want to standardize?, type "default" for using the default columns',
+                      'choose between:', str(data.columns)))
 
-        scaler = preprocessing.StandardScaler(self.X_train.loc[:, self.default_columns])
-        data.loc[:,default_columns] = scaler.transform(data.loc[:, self.default_columns])
+        scaler = preprocessing.StandardScaler().fit(self.X_train.loc[:, self.default_columns])
+        data.loc[:, self.default_columns] = scaler.transform(data.loc[:, self.default_columns])
         return data
 
-    def _history_test_data(self):
-        #function to add history data to the test set, since it is a time-series dataset
-        self.n_rows_to_append = self.skip_n_frames * self.time_window
-        history_before_test_set = np.array(self.x_train)[-self.n_rows_to_append:,:]
+    def _history_test_data(self, ):
+        # function to add history data to the test set, since it is a time-series dataset
+        # Needs modification!
+        history_before_test_set = np.array(self.x_train)[-self.batch_size:, :]
         test_data = np.array(self.x_test)
         test_data = np.concatenate(history_before_test_set, test_data)
         return test_data
-
-    def _skip_frames(self, skip_n_frames, data):
-        self.skip_n_frames = skip_n_frames
-        return np.array(data)[::skip_n_frames,:]
-
-    def _prepare_rolling_mean(self, time_window, data, is_training):
-        #handle corner cases when data%time_window !=0
-        #Returns Pandas DF
-        columns = data.columns
-        rows_in_last_section = len(data.index)%time_window
-        data_start = np.array(data[:-rows_in_last_section])
-        data_last_full_time_window = np.array(data[-time_window:])
-        data = pd.DataFrame(np.concatenate((data_start, data_last_full_time_window)))
-        data.columns = columns
-        return data
-
-    def _create_flattened_actions(self, prepared_rolling_mean_data):
-        prepared_data = self._prepare_rolling_mean(self.time_window, prepared_rolling_mean_data)
-        n_blocks = int(np.shape(prepared_data)[0]/self.time_window)
-        n_cols = time_window*3
-        actions_taken = np.array(prepared_data.loc[:, ['switch_U','switch_V', 'switch_W']])
-        split_data = np.array_split(actions_taken, n_blocks)
-        actions_taken_flattened = np.reshape(split_data, newshape=(n_blocks, n_cols))
-        return actions_taken_flattened
-
-
-    def _rolling_mean(self, time_window, data):
-        # USES PANDAS
-        self.time_window = time_window
-        data = data.loc[:,self.default_columns].groupby(np.arange(len(data.index))//time_window, axis=0).mean()
-        return data
-
-    def concatenate(self, rolling_mean_data, flattened_actions_data):
-        rolling_mean_data = np.array(rolling_mean_data)
-        flattened_actions_data = np.array(flattened_actions_data)
-        concatenated_data = np.concatenate((rolling_mean_data,flattened_actions_data), axis=1)
-        return concatenated_data
 
     def _select_batch(self, data):
         # want a full time_series, so can't use the final indexes in data[-self.batch_size:]
         length_data_set = np.shape(data)[0]
         time_series_suitable_indexes = length_data_set - self.batch_size
         index = np.random.randint(time_series_suitable_indexes)
-        batch = data[index:index+self.batch_size, :]
+        x_batch = data[index:index + self.batch_size - 1]
+        # target value is next time step, account for skip_n_frames and mean_window
+        y_batch = data[index + self.skip_n_frames * self.mean_window:index + self.batch_size - 1 + self.skip_n_frames * self.mean_window]
+        return x_batch, y_batch
+
+    def _skip_frames(self, data):
+        return data.loc[::self.skip_n_frames, :]
+
+    def _create_flattened_actions(self, batch):
+        n_blocks = int(np.shape(batch)[0] / self.mean_window)
+        n_cols = self.mean_window * 3
+        actions_taken = np.array(batch.loc[:, ['switch_U', 'switch_V', 'switch_W']])
+        split_data = np.array_split(actions_taken, n_blocks)
+        actions_taken_flattened = np.reshape(split_data, newshape=(n_blocks, n_cols))
+        actions_taken_flattened = pd.DataFrame(actions_taken_flattened)
+        names_list = [['U_T-' + str(i), 'V_T-' + str(i), 'W_T-' + str(i)] for i in range(self.mean_window)]
+        col_names_actions = list(itertools.chain(*names_list))
+        actions_taken_flattened.columns = col_names_actions
+        return actions_taken_flattened
+
+    def _concatenate(self, rolling_mean_data, flattened_actions_data):
+        concatenated_data = pd.concat((rolling_mean_data, flattened_actions_data), axis=1)
+        return concatenated_data
+
+    def _rolling_mean(self, batch):
+        # USES PANDAS
+        flattened_actions = self._create_flattened_actions(batch)
+        batch = batch.loc[:, self.default_columns].groupby(np.arange(len(batch.index)) // self.mean_window,
+                                                           axis=0).mean()
+        batch = self._concatenate(batch, flattened_actions)
         return batch
 
-    def _to_time_series(self,):
-        print('')
+    def _create_lag(self, batch):
+        history_data = []
+        batch_temp = batch[self.lag_period:]
+        index_values = batch_temp.index.values
+        for i in range(len(batch_temp)):
+            look_back = batch[i:(i + self.lag_period)]
+            look_back = np.reshape(np.array(look_back), newshape=(-1))
+            history_data.append(look_back)
+        history_data = pd.DataFrame(history_data)
+        history_data.reset_index(drop=True, inplace=True)
+        batch_temp.reset_index(drop=True, inplace=True)
+        lagged_data = pd.concat((batch_temp, history_data), axis=1)
+        lagged_data = lagged_data.set_index(index_values)
+        return lagged_data
+
+    def _preprocess(self, skip_frames=True, mean_window=True):
+        processed_data = self._remove_control_columns(self.X_train)
+        processed_data = self._standardize(processed_data)
+        x_batch, y_batch = self._select_batch(processed_data)
+        if skip_frames == True:
+            x_batch = self._skip_frames(data=x_batch)
+            y_batch = self._skip_frames(data=y_batch)
+        if mean_window == True:
+            x_batch = self._rolling_mean(x_batch)
+            y_batch = self._rolling_mean(y_batch)
+        x_batch = self._create_lag(x_batch)
+        y_batch = self._create_lag(y_batch)
+        return x_batch, y_batch.loc[:, self.default_columns]
 
 
+pipeline = Preprocessing(batch_size=64, mean_window=4, skip_n_frames=12,X_train=x_train, X_test=x_test, lag_period=10)
+x_batch, y_batch = pipeline._preprocess()
 
